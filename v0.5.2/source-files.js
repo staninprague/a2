bc7585febfb92520dd27921746da04b47261d853
Julius de Bruijn <julius.debruijn@360dialog.com>
2020-05-14 21:01:28 +0000

    rebuild pages at b2447a4313b7476f21e4140137b98a3ac04afbcc

---
+++ v0.5.2/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["a2"] = {"name":"","dirs":[{"name":"request","dirs":[{"name":"notification","files":["localized.rs","options.rs","plain.rs","silent.rs"]}],"files":["notification.rs","payload.rs"]}],"files":["client.rs","error.rs","lib.rs","request.rs","response.rs","signer.rs"]};
+createSourceSidebar();
